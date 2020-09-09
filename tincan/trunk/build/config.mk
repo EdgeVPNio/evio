3e4d6a0fba0292db5acf4c812d1e086fabc8870c
PrajwalaNagaraj <prajwalanagraj@gmail.com>
2020-09-09 17:14:57 -0400

    webrtc changes

--- tincan/trunk/build/config.mk
+++ tincan/trunk/build/config.mk
@@ -1,4 +1,4 @@
-CC = g++
+CC = clang++
 
 DEBUG ?= 0
 ifeq ($(DEBUG), 1)
@@ -14,6 +14,7 @@ ARCH = $(shell uname -m)
 INC_DIR = ../include
 INC_DIR_LNX = $(INC_DIR)/linux
 EXT_INC_DIR = ../../external/include
+RTC_INC_DIR = ../../external/include/webrtc
 
 SRC_DIR = ../src
 SRC_DIR_LNX = $(SRC_DIR)/linux
@@ -28,9 +29,9 @@ TARGET = $(patsubst %,$(BIN_DIR)/%,$(BINARY))
 
 defines = -DLINUX -D_TNC_LINUX -DWEBRTC_POSIX -DWEBRTC_LINUX -D_GLIBCXX_USE_CXX11_ABI=0
 
-cflags_cc += -std=c++14 -pthread -g2 -gsplit-dwarf -fno-strict-aliasing --param=ssp-buffer-size=4 -fstack-protector -funwind-tables -fPIC -pipe -Wall -fno-rtti
+cflags_cc += -std=c++14 --stdlib=libc++ -pthread -g2 -gsplit-dwarf -fno-strict-aliasing --param=ssp-buffer-size=4 -fstack-protector -funwind-tables -fPIC -pipe -Wall -fno-rtti
 
-LIBS = -ljsoncpp -lrtc_p2p -lrtc_base -lrtc_base_approved -lfield_trial_default -lboringssl -lboringssl_asm -lprotobuf_lite -lpthread -lutil
+LIBS = -nodefaultlibs -Wl,--start-group -lm -lgcc_s -lgcc -lc -lpthread -ljsoncpp -lyield_policy -llogging -lrtc_event -lprotobuf_lite -lutil -lchecks -lcriticalsection -lfield_trial_parser -lice_log -loptions -lplatform_thread_types -lrtc_pc_base -lsequence_checker -lstrings -lstun -ltimeutils -lweak_ptr -lsent_packet -lrtc_numerics -lthrow_delegate -lbase64 -lbad_optional_access -lmedia_transport_interface -lplatform_thread -lrtc_event_log -lrtc_media_base -lrtc_numerics -lrtp_parameters -lrtp_receiver -lrtp_rtcp_format -lthrow_delegate -lsrtp -llibjingle_peerconnection_api -lboringssl -lboringssl_asm -lrtc_base -lrtc_base_approved -lstringutils -ldata_rate -ldata_size -ltime_delta -lc++ -lc++abi -lrtc_p2p -lraw_logging_internal -lrtc_error -lfield_trial -lmetrics -lvideo_rtp_headers -ltask_queue -lfile_wrapper -Wl,--end-group
 
 HDR_FILES = $(wildcard $(INC_DIR)/*.h)
 SRC_FILES = $(wildcard $(SRC_DIR)/*.cc)
