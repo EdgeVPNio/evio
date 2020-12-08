20ffb4b1fc3507da6076eab5703718f30fbc8161
PrajwalaNagaraj <prajwalanagraj@gmail.com>
2020-12-08 14:20:37 -0500

    addressed and unit tested code review comments

--- tincan/trunk/src/tincan.cc
+++ tincan/trunk/src/tincan.cc
@@ -302,14 +302,17 @@ Tincan::Run()
   self_ = this;
   SetConsoleCtrlHandler(ControlHandler, TRUE);
 #endif // _TNC_WIN
+#if defined(_TNC_LINUX)
   //Registering signal with signal handler
   self_ = this;
-  struct sigaction newact, oldact;
+  struct sigaction newact;
+  memset(&newact, 0, sizeof(struct sigaction));
   newact.sa_handler = onStopHandler;
   sigemptyset(&newact.sa_mask);
   newact.sa_flags = 0;
-  sigaction(SIGINT, &newact, &oldact);
-
+  sigaction(SIGINT, &newact, NULL);
+  sigaction(SIGTERM, &newact, NULL);
+#endif
   //Start tincan control to get config from Controller
   unique_ptr<ControlDispatch> ctrl_dispatch(new ControlDispatch);
   ctrl_dispatch->SetDispatchToTincanInf(this);
@@ -363,12 +366,16 @@ Tincan::Shutdown()
   tunnels_.clear();
 }
 
+/*
+ * onStopHandler for handling SIGINT,SIGTERM in linux
+ * Calls OnStop() for shutdown of tincan
+ */
+#if defined(_TNC_LINUX)
 void
 Tincan::onStopHandler(int signum) {
-       cout << "Stopping tincan due to ctrl+c" << std::endl;
        self_->OnStop();
-       exit(signum);
 }
+#endif
 
 /*
 FUNCTION:ControlHandler
