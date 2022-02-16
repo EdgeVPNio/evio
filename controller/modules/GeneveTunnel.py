02f9684ea6117788a07853c4bb3e40ed20a4e01a
Pratiksha96 <jainpratiksha2110@gmail.com>
2022-02-16 21:31:08 +0000

    Test setup for geneve tunnel

--- controller/modules/GeneveTunnel.py
+++ controller/modules/GeneveTunnel.py
@@ -29,20 +29,13 @@ class GeneveTunnel(ControllerModule):
     def __init__(self, cfx_handle, module_config, module_name):
         super(GeneveTunnel, self).__init__(
             cfx_handle, module_config, module_name)
-<<<<<<< HEAD
-=======
         print("Geneve Tunnel constructor")
->>>>>>> Test setup for geneve tunnel
         self.ipr = IPRoute()
         self.ndb = NDB()
     
     def initialize(self):
-<<<<<<< HEAD
-        self.log("LOG_INFO", "Module loaded")
-=======
         self.logger.info("Module loaded")
         print("Config data: %s", self.config)
->>>>>>> Test setup for geneve tunnel
 
     def process_cbt(self, cbt):
         if cbt.op_type == "Request":
@@ -68,7 +61,6 @@ class GeneveTunnel(ControllerModule):
         pass
 
     def _create_geneve_tunnel(self, dev_name, id, remote_addr, dst_port=None):
-<<<<<<< HEAD
         try:            
             self.ipr.link("add",
                         ifname=dev_name,
@@ -78,28 +70,10 @@ class GeneveTunnel(ControllerModule):
         except Exception as e:
             self.log("LOG_INFO", "Error creating tunnel. Reported error: %s", str(e))
 
-    def _remove_geneve_tunnel(self, dev_name):
-        try:        
-            self.ipr.link("del", index=self.ipr.link_lookup(ifname=dev_name)[0])
-        except Exception as e:
-            self.log("LOG_INFO", "Error deleting tunnel. Reported error: %s", str(e))
-      
-    def _is_tunnel_exist(self, dev_name):
-        idx = self.ipr.link_lookup(ifname=dev_name)
-        if len(idx)==1:
-            return True
-=======
-        self.ipr.link("add",
-                      ifname=dev_name,
-                      kind="geneve",
-                      geneve_id=id,
-                      geneve_remote=remote_addr)
-
     def _del_tunnel(self, dev_name):
         self.ipr.link("del", index=self.ip.link_lookup(ifname=dev_name)[0])
       
     def _is_tunnel_exist(self):
->>>>>>> Test setup for geneve tunnel
         return False
 
     def req_handler_create_tunnel(self, cbt):
@@ -107,27 +81,18 @@ class GeneveTunnel(ControllerModule):
         remote_addr = cbt.request.params["RemoteAddr"]
         dst_port = cbt.request.params["DstPort"]
         dev_name = cbt.request.params["DeviceName"]
-<<<<<<< HEAD
         uid = cbt.request.params["UID"]
         if not self._is_tunnel_exist(dev_name):
             self._create_geneve_tunnel(
-                dev_name, uid, remote_addr, dst_port)
-            cbt.set_response(
+                dev_name, id, remote_addr, dst_port)
+            cbt.response.set_response(
                 data=f"Tunnel {dev_name} created", status=True)
         else:
             cbt.set_response(
-                data=f"Tunnel {dev_name} already exists", status=False)
+                data=f"Tunnel {dev_name} already exist", status=False)
 
     def req_handler_remove_tunnel(self, cbt):
         dev_name = cbt.request.params["DeviceName"]
-        if self._is_tunnel_exist(dev_name):
-            self._remove_geneve_tunnel(dev_name)
-            cbt.set_response(
-                data=f"Tunnel {dev_name} deleted", status=True)
-        else:
-            cbt.set_response(
-                data=f"Tunnel {dev_name} does not exists", status=False)
-=======
         vlid = cbt.request.params["VxLanID"]
         if not self._is_tunnel_exist():
             self._create_geneve_tunnel(
@@ -143,4 +108,4 @@ class GeneveTunnel(ControllerModule):
         self._del_tunnel(dev_name)
         cbt.response.set_response(
             data=f"Tunnel {dev_name} deleted", status=True)
->>>>>>> Test setup for geneve tunnel
+            
