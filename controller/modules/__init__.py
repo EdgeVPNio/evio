e3dc20f2266a41e0c7dbca1507633af8f157e8f7
Ken Subratie <kcratie@users.noreply.github.com>
2023-02-24 16:02:04 -0500

    Restructure layout

--- controller/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-
-def to_repr(obj):
-    return "{{{}}}".format(", ".join((f"\"{k}\": {obj.__dict__[k]!r}" for k in
-                                        (obj._REFLECT if hasattr(obj, "_REFLECT") else obj.__dict__.keys()))))
