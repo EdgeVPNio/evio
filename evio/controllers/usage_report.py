6c6a42c41e8d0e6acf99bd2e8c5e466a1e83d482
Ken Subratie <kcratie@users.noreply.github.com>
2023-03-17 19:53:38 -0400

    Formatting

--- evio/controllers/usage_report.py
+++ evio/controllers/usage_report.py
@@ -1,4 +1,4 @@
-﻿# EdgeVPNio
+# EdgeVPNio
 # Copyright 2020, University of Florida
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,7 +18,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-
 import hashlib
 import threading
 
